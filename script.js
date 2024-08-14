279ac01590728f390be421ed1ac3b9515a175e70
Samuel Cruz <samuelmarinhocruz1@gmail.com>
2024-08-14 19:54:33 +0000

    ainda um longo caminho

--- script.js
+++ script.js
@@ -4,22 +4,28 @@ function registrarValor() {
     var valor = document.getElementsByClassName("BMO")[0].value;
     var list = document.getElementsByClassName("tai")[0];
 
-    // Cria um novo item de lista
     var listItem = document.createElement("li");
     listItem.id = `item-${idCounter}`;
     listItem.innerHTML = `
         ${valor}
+        <button onclick="alterarValor('item-${idCounter}')" class="diva" class="altere">Editar</button>
         <button onclick="deletarValor('item-${idCounter}')" class="diva">-</button>
     `;
 
-    // Adiciona o novo item à lista
     list.appendChild(listItem);
 
-    // Incrementa o contador de IDs
     idCounter++;
 }
 
 function deletarValor(id) {
     var item = document.getElementById(id);
     item.remove();
+}
+
+function alterarValor() {
+    var valores = document.getElementsByClassName("tai");
+    valores.innerHTML = `
+        <input type="text">
+        <Button>Ok</Button>
+    `
 }
